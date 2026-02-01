93aa3c936921997a9db84a815f59b760d76a52e4
May Dan <its.danmay@gmail.com>
2026-02-01 19:18:02 +0200

    show restaurants and bars

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useMemo, useRef, useState } from "react";
 import "./App.css";
-import { useDebounceValue } from "./useDebounce";
+import { useDebounce, useDebounceValue } from "./useDebounce";
 
 interface City {
   id: number;
@@ -9,6 +9,8 @@ interface City {
 
 function App() {
   const [cities, setCities] = useState<City[] | []>([]);
+  const [cityRestaurants, setCityRestaurants] = useState([]);
+  const [cityBars, setCityBars] = useState([]);
   const [searchInput, setSearchInput] = useState("");
 
   const debounceValue = useDebounceValue(searchInput, 2000);
@@ -20,14 +22,46 @@ function App() {
     );
   }, [debounceValue, cities]);
 
+  const fetchCityRestaurants = async (str, signal) => {
+    const response = await fetch(
+      `http://localhost:3000/api/cities/${str.toLocaleLowerCase()}/restaurants`,
+      { signal }
+    );
+    const newCityRestaurants = await response.json();
+    setCityRestaurants(newCityRestaurants);
+  };
+
+  const fetchCityBars = async (str, signal) => {
+    const response = await fetch(
+      `http://localhost:3000/api/cities/${str.toLocaleLowerCase()}/bars`,
+      { signal }
+    );
+    const newCityBars = await response.json();
+    setCityBars(newCityBars);
+  };
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const response = await fetch("http://localhost:3000/api/cities");
+  //     const data = await response.json();
+  //     setCities(data);
+  //   };
+  //   fetchData();
+  // }, []);
+
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch("http://localhost:3000/api/cities");
-      const data = await response.json();
-      setCities(data);
-    };
-    fetchData();
-  }, []);
+    if (searchInput.length === 0) {
+      return;
+    }
+    const controller = new AbortController();
+    const signal = controller.signal;
+    Promise.all([
+      fetchCityRestaurants(debounceValue, signal),
+      fetchCityBars(debounceValue, signal),
+    ]);
+
+    return () => controller.abort();
+  }, [debounceValue]);
 
   return (
     <>
@@ -37,10 +71,17 @@ function App() {
         value={searchInput}
         onChange={(e) => setSearchInput(e.target.value)}
       />
+      <div>Restaurants:</div>
       <ul>
-        {filtered.map((city) => (
-          <li key={city.id}>{city.name}</li>
-        ))}
+        {cityRestaurants.length > 0 &&
+          cityRestaurants.map((restaurant) => (
+            <li key={restaurant.id}>{restaurant.name}</li>
+          ))}
+      </ul>
+      <div>Bars:</div>
+      <ul>
+        {cityBars.length > 0 &&
+          cityBars.map((bar) => <li key={bar.id}>{bar.name}</li>)}
       </ul>
     </>
   );
